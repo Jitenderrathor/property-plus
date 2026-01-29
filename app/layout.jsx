a850a9f86f88f780ea12c996af66f1e80d714cd1
JitenderRathor <jitenderjkr19@gmail.com>
2026-01-30 00:00:58 +0530

    two section added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,24 @@
-import '@/assets/styles/globals.css'
-
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: 'Property Plus',
-  keywords: 'real estate, property, homes, apartments, buying, selling, renting',
-  description: 'Discover Your Dream Home with Property Plus - Your Ultimate Real Estate Companion',
-}
+  title: "Property Plus",
+  keywords:
+    "real estate, property, homes, apartments, buying, selling, renting",
+  description:
+    "Discover Your Dream Home with Property Plus - Your Ultimate Real Estate Companion",
+};
 function MainLayout({ children }) {
   return (
     <html>
-        <body>
-            <main>{children}</main>
-        </body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
---
+++ components/InfoBox.jsx
@@ -0,0 +1,5 @@
+const InfoBox = () => {
+  return <div className="info-box">Info Box</div>;
+};
+
+export default InfoBox;
