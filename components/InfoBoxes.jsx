4e9a9d738fbb71e861b4b7cefcf5118468b77001
JitenderRathor <jitenderjkr19@gmail.com>
2026-01-31 00:18:58 +0530

    home ccomplete

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,36 +1,28 @@
-import Link from "next/link";
+import InfoBox from "./InfoBox";
 
 const InfoBoxes = () => {
   return (
     <section>
       <div class="container-xl lg:container m-auto">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div class="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold">For Renters</h2>
-            <p class="mt-2 mb-4">
-              Find your dream rental property. Bookmark properties and contact
-              owners.
-            </p>
-            <Link
-              href="/properties"
-              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Properties
-            </Link>
-          </div>
-          <div className="bg-blue-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Property Owners</h2>
-            <p className="mt-2 mb-4">
-              List your properties and reach potential tenants. Rent as an
-              airbnb or long term.
-            </p>
-            <Link
-              href="/properties/add"
-              class="inline-block bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600"
-            >
-              Add Property
-            </Link>
-          </div>
+          <InfoBox
+            heading="For Renters"
+            pera="Find your dream rental property. Bookmark properties and contact owners."
+            background="bg-gray-100"
+            textColor="text-gray-800"
+            buttonText="Browse Properties"
+            buttonLink="/properties"
+            buttonBgColor="bg-black"
+          />
+          <InfoBox
+            heading="For Property Owners"
+            pera="List your properties and reach potential tenants. Rent as an airbnb or long term."
+            background="bg-blue-100"
+            textColor="text-gray-800"
+            buttonText="Add Property"
+            buttonLink="/properties/add"
+            buttonBgColor="bg-blue-600"
+          />
         </div>
       </div>
     </section>
