e9151271891ea81bc818aa13e3f54ef453bcbc43
JitenderRathor <jitenderjkr19@gmail.com>
2026-02-05 23:08:41 +0530

    Property page added

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,3 +1,6 @@
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+
 export const metadata = {
   title: "Properties - Property Plus",
   keywords:
@@ -7,7 +10,21 @@ export const metadata = {
 };
 
 function Properties() {
-  return <div>Properties</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found.</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default Properties;
